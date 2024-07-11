48b70f126b2d21e59c41763e01203b82e2746637
Onestyle7 <klusbartek8@gmail.com>
2024-07-11 19:39:21 +0200

    Added OperationHttpResponseData.cs which define structure of http response

---
+++ Backend/Operations/OperationHttpResponseData.cs
@@ -0,0 +1,31 @@
+﻿namespace DevBuild.Backend.Operations
+{
+    public class OperationHttpResponseData<T> where T : class
+    {
+
+        public static OperationHttpResponseData<T> FromOperationResult(OperationResult<T> result, bool exposeData = false)
+        {
+            var responseData = new OperationHttpResponseData<T>()
+            {
+                ErrorCode = result.ErrorCode.ToString()
+            };
+
+            if (exposeData)
+            {
+                responseData.Data = result.Data;
+            }
+
+            return responseData;
+        }
+
+        public string ErrorCode
+        {
+            get; private set;
+        }
+
+        public object Data
+        {
+            get; private set;
+        }
+    }
+}
